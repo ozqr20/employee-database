1f390479b3f06cae956e941611ebe5514ce347f5
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-09-04 10:48:09 -0400

    updated seeds.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,22 @@
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50)
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NULL
 );
 
 CREATE TABLE role (
     department_id INTEGER,
-    title VARCHAR(50),
+    title VARCHAR(50) NULL,
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    salary DECIMAL,
+    salary DECIMAL(10,4) NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
+    first_name VARCHAR(50) NULL,
+    last_name VARCHAR(50) NULL,
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    role_id INTEGER,
-    manager_id INTEGER,
+    role_id INTEGER NULL,
+    manager_id INTEGER NULL,
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN key (manager_id) REFERENCES employee(id)
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,25 +1,24 @@
-INSERT INTO department(name) 
-VALUES
-('Marketing'),
-('Finance'),
-('Security'),
-("Development"),
-("Legal"),
-("Sales"),
-("Customer Service");
+-- Departments 
+INSERT INTO department(name) VALUES ('Marketing'),
+INSERT INTO department(name) VALUES ('Finance'),
+INSERT INTO department(name) VALUES ('Security'),
+INSERT INTO department(name) VALUES ("Development"),
+INSERT INTO department(name) VALUES ("Legal"),
+INSERT INTO department(name) VALUES ("Sales"),
+INSERT INTO department(name) VALUES ("Customer Service");
 
-INSERT INTO role(title, salary,department_id)
-VALUES
-('Marketing Lead', 103000, 1),
-('Finance Associate', 130000, 2),
-('Cybersecurity', 90000, 3),
-('Developer', 95000, 4),
-('CS Manager', 75000, 7);
+-- Roles
+INSERT INTO role(title, salary,department_id) VALUES ('Marketing Lead', 103000, 1),
+INSERT INTO role(title, salary,department_id) VALUES ('Finance Associate', 130000, 2),
+INSERT INTO role(title, salary,department_id) VALUES ('Cybersecurity', 90000, 3),
+INSERT INTO role(title, salary,department_id) VALUES ('Developer', 95000, 4),
+INSERT INTO role(title, salary,department_id) VALUES ('CS Manager', 75000, 7);
 
-INSERT INTO employee(first_name, last_name, manager_id, role_id)
-VALUES
-('KELVIN', 'GUESS', 1,2),
-('MIKE', 'TYSON', 2,3),
-('DAVIE', 'BUSTERS', 2, 3),
-('HOPPER', 'IHOP', NULL, 5),
-('RAMBO', 'SMITH', 2, 4);
+-- Employees
+INSERT INTO employee(first_name, last_name, manager_id, role_id) VALUES ('KELVIN', 'GUESS', 1,2),
+INSERT INTO employee(first_name, last_name, manager_id, role_id) VALUES ('MIKE', 'TYSON', 2,3),
+INSERT INTO employee(first_name, last_name, manager_id, role_id) VALUES ('DAVIE', 'BUSTERS', 2, 3),
+INSERT INTO employee(first_name, last_name, manager_id, role_id) VALUES ('HOPPER', 'IHOP', NULL, 5),
+INSERT INTO employee(first_name, last_name, manager_id, role_id) VALUES ('RAMBO', 'SMITH', 2, 4);
+INSERT INTO employee(first_name, last_name, role_id) VALUES ('MARIA', 'CROWREN', 4);
+INSERT INTO employee(first_name, last_name, role_id) VALUES ('IVAN', 'MELO', 2);
